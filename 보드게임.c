413e46338f0f760f27ee26ae3ee06fc87254fd14
eunjin2118 <80873499+eunjin2118@users.noreply.github.com>
2021-05-13 15:05:32 +0900

    주사위 엔터로 던지기

--- 보드게임.c
+++ 보드게임.c
@@ -16,12 +16,32 @@ void main(void)
 {
 	//칸의 최대갯수
 	int max_length = 61;
+
+	//플레이어의 위치
+	int position = 0;
+
 	//주사위
 	int dice;
 
-	srand( time(0) );
+	srand(time(0));
 	rand();
-	dice = rand() % 6 + 1;
 
-	printf("%d\n", dice);
+	printf("현재위치 : %d\n\n", position);
+	getchar();
+
+	//주사위 던지기 (1부터 6까지)
+	dice = rand() % 6 + 1;
+	position = position + dice;
+	printf("현재위치 : %d\n\n", position);
+	getchar();
+
+	dice = rand() % 6 + 1;
+	position = position + dice;
+	printf("현재위치 : %d\n\n", position);
+	getchar();
+
+	dice = rand() % 6 + 1;
+	position = position + dice;
+	printf("현재위치 : %d\n\n", position);
+	getchar();
 }
