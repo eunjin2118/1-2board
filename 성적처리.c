691045801d82a5a01832fc91454497ba7439a7bf
eunjin2118 <80873499+eunjin2118@users.noreply.github.com>
2021-08-31 15:09:44 +0900

    함수화(refactoring)

--- 성적처리.c
+++ 성적처리.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-void main(void)
+//입력받은 점수(score)에 대한 등급을 출력한다
+void cal_grade(int score)
 {
-	int score;
-	printf("점수를 입력하세요 : ");
-	scan_sf("%d", &score);
-
 	if (90 <= score && score <= 100)
-		printf("A입니다 \n")
+		printf("A입니다 \n");
 	else if (80 <= score)
 		printf("B입니다 \n");
 	else if (70 <= score)
@@ -16,4 +13,27 @@ void main(void)
 		printf("D입니다 \n");
 	else
 		printf("E입니다 \n");
+}
+
+void main(void)
+{
+	int C_score;
+	printf("씨점수를 입력하세요 : ");
+	scanf_s("%d", &C_score);
+
+	cal_grade(C_score);
+	
+
+	int java_score;
+	printf("자바점수를 입력하세요 : ");
+	scanf_s("%d", &java_score);
+
+	cal_grade(java_score);
+
+
+	int CA_score;
+	printf("컴퓨터구조 점수를 입력하세요 : ");
+	scanf_s("%d", &CA_score);
+
+	cal_grade(CA_score);
 }
