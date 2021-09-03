b7ee4d4977c57375cfc830641bca5fbeff28edae
eunjin2118 <80873499+eunjin2118@users.noreply.github.com>
2021-09-03 14:57:00 +0900

    배열적용1

--- 성적처리.c
+++ 성적처리.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 //입력받은 점수(score)에 대한 등급을 출력한다
 void cal_grade(int score);
 
 double cal_average(int score_a, int score_b, int score_c);
 
-void main(void) 
+void main(void)
 {
-	int C_score;
+	int score[3];
+
 	printf("씨점수를 입력하세요 : ");
-	scanf_s("%d", &C_score);
-	cal_grade(C_score);
-	int java_score;
+	scanf_s("%d", &score[0]);
+	cal_grade(score[0]);
+
 	printf("자바점수를 입력하세요 : ");
-	scanf_s("%d", &java_score);
-	cal_grade(java_score);
-	int CA_score;
+	scanf_s("%d", &score[1]);
+	cal_grade(score[1]);
+
 	printf("컴퓨터구조 점수를 입력하세요 : ");
-	scanf_s("%d", &CA_score);
-	cal_grade(CA_score);
+	scanf_s("%d", &score[2]);
+	cal_grade(score[2]);
 
 	double average;
-	average = cal_average(C_score, java_score, CA_score);
+	average = cal_average(score[0], score[1], score[2]);
 	printf("세 과목의 평균은 %f입니다", average);
 }
 
@@ -42,5 +45,6 @@ void cal_grade(int score)
 //세 과목의 평균을 반환
 double cal_average(int score_a, int score_b, int score_c)
 {
-	return (double)(score_a+score_b+score_c)/(double)3;
+	return (double)(score_a + score_b + score_c) / (double)3;
 }
+
